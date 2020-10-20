197d5e7739c6b5efe2ca5fba713ff13739a44808
Mirwais Shahryar <69811338+mirwaisfe20@users.noreply.github.com>
2020-10-20 11:04:19 +0200

    Add files via upload

---
+++ domChalleneg1.js
@@ -0,0 +1,14 @@
+/* 
+    change the body color to : #C6E2FF
+    change the body font color to : #ffffff;
+    give height of '2rem' to p with class "title" element;
+    give Margin bottom of 'rem' to p with class "title" element;
+
+    user querySelectorAll to select all article elements
+    loop through the elements from the nodelit
+
+    TIP: nodelist = array
+
+    change the background color of each odd element to: #1464F4;
+    change the background color of each even element to: #120A8F;
+*/
